1054daed7aaa1c70e90cf283d93cd566b40f6f4f
Antonio Neto <antonio.neto.cost@gmail.com>
2025-04-02 20:56:12 -0300

    esboço cenário e pequena correção na classe Player

--- Jogo Luta/Assets/Scripts/player.cs
+++ Jogo Luta/Assets/Scripts/player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float velocidade_movimento;
     public float forca_pulo;
