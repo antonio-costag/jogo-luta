c0b1eaf03d7161d7013b097967e56e90431d2c41
Antonio Neto <antonio.neto.cost@gmail.com>
2025-04-06 21:28:26 -0300

    Separação e organização dos métodos da classe Player

--- Jogo Luta/Assets/Scripts/player.cs
+++ Jogo Luta/Assets/Scripts/player.cs
@@ -13,21 +13,31 @@ public class Player : MonoBehaviour
     private bool colidir_chao = false;
     private bool esta_atacando = false;
     public Animator animatorPlayer;
+    public int vidaMax;
+    private int vida;
+
+
+    //GRUPO: METODOS UNITY
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        vida = vidaMax;
     }
     void Update()
     {
         //Pulo();
         ScaleSprit();
         Ataque();
+        print(vida);
     }
     void FixedUpdate()
     {
         VerificarMovimento();
         // Move o jogador baseado na física
     }
+
+
+    //GRUPO: METODOS ENVOLVENDO MOVIMENTAÇÃO
     void VerificarMovimento(){
         if(esta_atacando == false){
             Movimento();
@@ -44,48 +54,6 @@ public class Player : MonoBehaviour
         Vector2 movimentos = new Vector2(input_x, rb.velocity.y);
         rb.velocity = movimentos;
         AnimacaoAndar();
-    }
-    void Pulo(){
-        if(Input.GetButtonDown("Jump") && colidir_chao == true){
-            rb.AddForce(new Vector2(0, forca_pulo), ForceMode2D.Impulse);
-            //ForceMode2D.Impulse, efeito de impulso
-        }
-    }
-    void Ataque()
-    {
-        if (Input.GetMouseButtonDown(0) && esta_atacando == false)
-        {
-            esta_atacando = true;
-            animatorPlayer.SetBool("ataque_simples", true);
-            animatorPlayer.SetBool("andar", false);
-            //Iniciando a animação de ataque
-        }
-    }
-    void EncerrarAtaque(){
-        animatorPlayer.SetBool("ataque_simples", false);
-        esta_atacando = false;
-        //Interrompendo a animação de ataque
-    }
-    bool DanoInimigo(){
-        //Verifica se o Inimigo pode levar dano
-        if(GameObject.FindGameObjectWithTag("Espada").activeSelf){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    void ProcurarInimigoColisao(Collider2D Collision){
-        //Pega o script do gerador de inimigos
-        GeradorInimigos gerador = FindObjectOfType<GeradorInimigos>();
-        //Contains verifica se o objeto colidido é um inimigo gerado
-        if (gerador.ListinimigosGerados.Contains(Collision.gameObject))
-        {
-            Collider2D CollicionInimigo = Collision;
-            if(DanoInimigo()){
-                CollicionInimigo.gameObject.GetComponent<Inimigo>().DestruirInimigo();
-            }
-        }
     }
     void ScaleSprit(){
         if(esta_atacando == false){
@@ -106,6 +74,59 @@ public class Player : MonoBehaviour
         }
         //Se o jogador estiver se movendo, a animação de andar é ativada
     }
+    void Pulo(){
+        if(Input.GetButtonDown("Jump") && colidir_chao == true){
+            rb.AddForce(new Vector2(0, forca_pulo), ForceMode2D.Impulse);
+            //ForceMode2D.Impulse, efeito de impulso
+        }
+    }
+
+
+    //GRUPO: METODOS ENVOLVENDO COMBATE
+    void Ataque()
+    {
+        if (Input.GetMouseButtonDown(0) && esta_atacando == false)
+        {
+            esta_atacando = true;
+            animatorPlayer.SetBool("ataque_simples", true);
+            animatorPlayer.SetBool("andar", false);
+            //Iniciando a animação de ataque
+        }
+    }
+    void EncerrarAtaque(){
+        animatorPlayer.SetBool("ataque_simples", false);
+        esta_atacando = false;
+        //Interrompendo a animação de ataque
+    }
+    bool DanoAoInimigo(){
+        //Verifica se o Inimigo pode levar dano
+        if(GameObject.FindGameObjectWithTag("Espada").activeSelf){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    //GRUPO: METODOS ENVOLVENDO COLISÃO
+     void VidaPLyer(Collision2D collision){
+        if(collision.gameObject.tag == "Inimigo"){
+            vida -= vidaMax / 10;
+        }
+    }
+    void ProcurarInimigoColisao(Collider2D Collision){
+        //Pega o script do gerador de inimigos
+        GeradorInimigos gerador = FindObjectOfType<GeradorInimigos>();
+        //Contains verifica se o objeto colidido é um inimigo gerado
+        if (gerador.ListinimigosGerados.Contains(Collision.gameObject))
+        {
+            Collider2D CollicionInimigo = Collision;
+            if(DanoAoInimigo()){
+                CollicionInimigo.gameObject.GetComponent<Inimigo>().DestruirInimigo();
+            }
+        }
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         ProcurarInimigoColisao(collision);
@@ -113,16 +134,16 @@ public class Player : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        VidaPLyer(collision);
+
+        //Verifica se o jogador colidiu com o chão
         if(collision.gameObject.tag == "Chao"){
             colidir_chao = true;
-        }
-        if(collision.gameObject.tag == "Inimigo"){
-            print("dano");
         }
     }
     void OnCollisionExit2D(Collision2D collision)
     {
-        //Verifica se o jogador está colidindo com o chão
+        //Verifica se o jogador deixou de colidir com o chão
         if(collision.gameObject.tag == "Chao"){
             colidir_chao = false;
         }
